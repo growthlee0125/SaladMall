b73d83593b437e8a3afa87545931cd9a39443683
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2022-01-06 14:38:49 +0900

    Update R_Custom_Controller.java

--- src/main/java/com/teamsalad/controller/R_Custom_Controller.java
+++ src/main/java/com/teamsalad/controller/R_Custom_Controller.java
@@ -19,7 +19,6 @@ import com.teamsalad.service.R_CustomService;
 @RequestMapping("/R_Custom/*")
 public class R_Custom_Controller {
 	
-	// 서비스 작성 후 사용
 	@Inject
 	private R_CustomService service;
 	
@@ -29,26 +28,27 @@ public class R_Custom_Controller {
 	// 메인 화면 호출(GET)
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String mainGET(HttpSession session) throws Exception{
+		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 
 		// R_Custom 메인 페이지 호출 체크
 		logger.info("커스텀 샐러드 페이지 호출");
-		// /R_Custom/main.jsp
-		
+				
 		return "/R_Custom/main";
 	}
 	
 	// 새로운 샐러드 만들기
 	@RequestMapping(value = "/main", method = RequestMethod.POST)
 	public String mainPOST(HttpSession session) throws Exception{
-		
 
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
@@ -63,95 +63,91 @@ public class R_Custom_Controller {
 	}
 	
 	// http://localhost:8080/R_Custom/step1	
-	// 스텝1
+	// 스텝1 - 야채 선택
 	@RequestMapping(value = "/step1", method = RequestMethod.GET)
 	public String step1GET(HttpSession session, Model model) throws Exception{
 		
-		// 야채 선택
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
 		logger.info("야채 선택 페이지 호출");
 		
-		// 서비스 동작 호출
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(1));
+		
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 
 		return "/R_Custom/step1";
 	}
 	
+	// step-1 등록
 	@RequestMapping(value = "/step1", method = RequestMethod.POST)
 	public String step1POST(HttpSession session, customVO vo, Integer igdt_num, Model model) throws Exception{
 		
 		logger.info("step1POST() 호출 !!");
 		
-		
-		// 재료 추가
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
-				
-		vo.setM_id(m_id);
-		vo.setRcp_cmbnt(""+igdt_num);
 		
+		// 재료 추가
+		vo.setM_id(m_id);
+		vo.setRcp_cmbnt(""+igdt_num);		
 		service.addCustom(vo);
 		
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(1));
+		
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step1";
 	}
 	
-	
 	// http://localhost:8080/R_Custom/step2
-	// 스텝2
+	// 스텝2 - 고기 선택
 	@RequestMapping(value = "/step2", method = RequestMethod.GET)
 	public String step2GET(HttpSession session, Model model) throws Exception{
 		
-		// 고기 선택
 		logger.info("고기 선택 페이지 호출");
 		
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
-		// 서비스 동작 호출
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(2));
+		
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step2";
-		
 	}
 	
+	// 스텝 2 - 고기 등록
 	@RequestMapping(value = "/step2", method = RequestMethod.POST)
 	public String step2POST(HttpSession session, customVO vo, Integer igdt_num, Model model) throws Exception{
 		
 		logger.info("step2POST() 호출 !!");
 		
-		// 재료 추가
-		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
-		
 		vo.setM_id(m_id);
 		vo.setRcp_cmbnt(""+igdt_num);
-		
 		service.addCustom(vo);
 		
 		// DB에서 가져온 재료 view 페이지로 전달
@@ -163,43 +159,41 @@ public class R_Custom_Controller {
 	}
 	
 	// http://localhost:8080/R_Custom/step3
-	// 스텝3
+	// 스텝3 - 해산물 선택
 	@RequestMapping(value = "/step3", method = RequestMethod.GET)
 	public String step3GET(HttpSession session, Model model) throws Exception{
 		
-		// 소스 선택
-		logger.info("소스 선택 페이지 호출");
-		
+		logger.info("해산물 선택 페이지 호출");
 
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
-		// 서비스 동작 호출
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(3));
+		
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step3";
 	}
 	
+	// 스텝3 - 해산물 등록
 	@RequestMapping(value = "/step3", method = RequestMethod.POST)
 	public String step3POST(HttpSession session, customVO vo, Integer igdt_num, Model model) throws Exception{
 		
 		logger.info("step3POST() 호출 !!");
 		
-		// 재료 추가
-		
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
 		vo.setM_id(m_id);
 		vo.setRcp_cmbnt(""+igdt_num);
-		
 		service.addCustom(vo);
 		
 		// DB에서 가져온 재료 view 페이지로 전달
@@ -211,44 +205,40 @@ public class R_Custom_Controller {
 	}
 	
 	// http://localhost:8080/R_Custom/step4
-	// 스텝4
+	// 스텝4 - 소스 선택
 	@RequestMapping(value = "/step4", method = RequestMethod.GET)
 	public String step4GET(HttpSession session, Model model) throws Exception{
 		
-		// 소스 선택
 		logger.info("소스 선택 페이지 호출");
-		
 
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
-		// 서비스 동작 호출
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(4));
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step4";
-		
 	}
 	
+	// 스텝4 - 소스 등록
 	@RequestMapping(value = "/step4", method = RequestMethod.POST)
 	public String step4POST(HttpSession session, customVO vo, Integer igdt_num, Model model) throws Exception{
 		
 		logger.info("step4POST() 호출 !!");
 		
-		// 재료 추가
-		
-
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
+		
 		vo.setM_id(m_id);
 		vo.setRcp_cmbnt(""+igdt_num);
-		
 		service.addCustom(vo);
 		
 		// DB에서 가져온 재료 view 페이지로 전달
@@ -257,48 +247,46 @@ public class R_Custom_Controller {
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step4";
-		
 	}
 	
 	// http://localhost:8080/R_Custom/step5
-		// 스텝5
+	// 스텝 5 - 기타 선택
 	@RequestMapping(value = "/step5", method = RequestMethod.GET)
 	public String step5GET(HttpSession session, Model model) throws Exception{
 		
-		// 소스 선택
-		logger.info("소스 선택 페이지 호출");
+		logger.info("기타 선택 페이지 호출");
 		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
-		// 서비스 동작 호출
 		// DB에서 가져온 재료 view 페이지로 전달
 		model.addAttribute("ingredientList", service.igdtList(5));
+		
 		// 현재 샐러드 보여주기
 		model.addAttribute("customList", service.customList(m_id));
 		
 		return "/R_Custom/step5";
 	}
 	
+	// 스텝5 - 기타 등록
 	@RequestMapping(value = "/step5", method = RequestMethod.POST)
 	public String step5POST(HttpSession session, customVO vo, Integer igdt_num, Model model) throws Exception{
 		
 		logger.info("step5POST() 호출 !!");
 		
-		// 재료 추가
-		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
 		vo.setM_id(m_id);
 		vo.setRcp_cmbnt(""+igdt_num);
-		
 		service.addCustom(vo);
 		
 		// DB에서 가져온 재료 view 페이지로 전달
@@ -309,7 +297,8 @@ public class R_Custom_Controller {
 		return "/R_Custom/step5";
 
 	}
-
+	
+	// 레시피 등록 페이지 호출
 	@RequestMapping(value = "/addRecipe", method = RequestMethod.GET)
 	public String addRecipeGET(HttpSession session, Model model) throws Exception{
 		
@@ -317,6 +306,7 @@ public class R_Custom_Controller {
 		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
@@ -332,6 +322,7 @@ public class R_Custom_Controller {
 		return "/R_Custom/addRecipe";
 	}
 	
+	// 레시피 등록하기
 	@RequestMapping(value = "/addRecipe", method = RequestMethod.POST)
 	public String addRecipePOST(HttpSession session, recipeVO vo) throws Exception{
 		
@@ -339,6 +330,7 @@ public class R_Custom_Controller {
 		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
@@ -348,9 +340,11 @@ public class R_Custom_Controller {
 		
 		// R_Custom/main으로 이동
 		logger.info("저장 완료"+vo);
+		
 		return "redirect:/R_Custom/main";
 	}
 	
+	// 장바구니 페이지 호출
 	@RequestMapping(value = "/addCart", method = RequestMethod.GET)
 	public String addCartGET(HttpSession session, Model model) throws Exception{
 		
@@ -358,6 +352,7 @@ public class R_Custom_Controller {
 		
 		// 세션값(m_id) 가져오기
 		String m_id = (String) session.getAttribute("m_id");
+		
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
@@ -372,38 +367,29 @@ public class R_Custom_Controller {
 		return null;
 	}
 	
+	// 장바구니 등록
 	@RequestMapping(value = "/addCart", method = RequestMethod.POST)
 	public String addCartPOST(String m_id, String rcp_cmbnt) throws Exception{
+		
 		logger.info(" addCartPOST() 호출 ");
 		
-
 		// 세션값(m_id) 가져오기
 		if(m_id==null) {
 			return "redirect:/M_Login/login";
 		}
 		
 		cartVO vo = new cartVO();
-		
 		recipeVO rvo = service.findRecipe(rcp_cmbnt);
 		
-		logger.info("찾은 값은 "+rvo);
 		vo.setM_id(m_id);
 		vo.setRcp_num(rvo.getRcp_num());
 		
 		// 레시피 가격
-		
-		
-		
 		vo.setCart_price(rvo.getRcp_price());
 		
-		logger.info("전송할 값은 "+vo);
 		// tbl_cart에 추가
 		service.addCart(vo);
 		
-		
 		return "redirect:/Basket/basket";
-	}
-	
-	
-	
+	}	
 }
