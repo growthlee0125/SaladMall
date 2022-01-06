6dc7c59181cf7b0442926bf01f0c95dc59a27481
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2022-01-06 15:01:54 +0900

    Update R_BoardDAOImpl.java

--- src/main/java/com/teamsalad/persistence/R_BoardDAOImpl.java
+++ src/main/java/com/teamsalad/persistence/R_BoardDAOImpl.java
@@ -27,9 +27,9 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	private static final String namespace = "com.teamsalad.mapper.R_BoardMapper";
 	
-	
 	// 게시물 등록하기
 	public void r_Board_regist(recipeBoardVO vo) throws Exception {
+		
 		logger.info(" DAO : board regist 시작");
 			
 		sqlSession.insert(namespace + ".R_Board_regist", vo);
@@ -37,8 +37,9 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 		logger.info(" DAO : board regist 끝");
 	}
 	
-
+	// 레시피 가져오기
 	public recipeVO R_BoardgetRecipe(int rcp_num) throws Exception{
+		
 		logger.info(" DAO : board regist 시작");
 		
 		recipeVO vo = sqlSession.selectOne(namespace + ".R_BoardgetRecipe", rcp_num);
@@ -50,6 +51,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 게시물 수정하기
 	public void r_Board_modify(recipeBoardVO vo) throws Exception {
+		
 		logger.info(" DAO : board modify 시작");
 		
 		sqlSession.update(namespace + ".R_Board_modify", vo);
@@ -59,13 +61,13 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 게시물 삭제
 	public void r_Board_delete(int rcp_b_num) throws Exception {
+		
 		logger.info(" DAO : board delete 시작");
 		
 		sqlSession.insert(namespace + ".R_Board_delete", rcp_b_num);
 		
 		logger.info(" DAO : board delete 끝");
 	}
-	
 	
 	// 게시물 가져오기
 	public BoardMemberVO r_Board_detail(int rcp_b_num) throws Exception {
@@ -83,12 +85,11 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 재료 이름 가져오기
 	public String r_Board_getIgdtName(int igdt) throws Exception {
+		
 		logger.info(" DAO : getIgdtName 시작");
 		
 		String name = sqlSession.selectOne(namespace + ".R_Board_getIgdtName", igdt);
 			
-		logger.info(" DAO : 결과확인 = " + name);
-		
 		logger.info(" DAO : getIgdtName 끝");
 		
 		return name;
@@ -105,10 +106,6 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 		
 		List<BoardMemberVO> vo = sqlSession.selectList(namespace + ".R_Board_listPage", map);
 		
-		System.out.println("DAO 결과확인 : " + vo);
-		
-		logger.info(" DAO : 결과확인 = " + vo.size());
-		
 		logger.info(" DAO : board listPage 끝");
 		
 		return vo;
@@ -116,6 +113,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 주간 인기 게시물 가져오기
 	public List<BoardMemberVO> r_Board_weeklyPopular() throws Exception{
+		
 		logger.info(" DAO : board weeklyPopular 시작");
 		
 		List<BoardMemberVO> vo = sqlSession.selectList(namespace + ".R_Board_weeklyPopular");
@@ -129,6 +127,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 모든 게시물 가져오기
 	public List<BoardMemberVO> r_Board_listTotal() throws Exception {
+		
 		logger.info(" DAO : board listTotal 시작");
 		
 		logger.info(" DAO : 결과확인 = " + sqlSession.selectList(namespace + ".R_Board_listTotal"));
@@ -140,7 +139,9 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 좋아요 누른지 확인하기
 	public boolean r_Board_likeCheck(int rcp_b_num, String m_id) throws Exception {
+		
 		Map<String, Object> map = new HashMap<String, Object>();
+		
 		map.put("rcp_b_num", rcp_b_num);
 		map.put("m_id", m_id);
 		
@@ -158,6 +159,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 좋아요 누른 후 게시물 좋아요 개수 업데이트
 	public void r_Board_likeUpdate(int rcp_b_num) throws Exception {
+		
 		logger.info(" DAO : board likeUpdate 시작");
 		
 		sqlSession.update(namespace + ".R_Board_likeUpdate", rcp_b_num);
@@ -167,6 +169,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 좋아요 테이블에 좋아요 삽입
 	public void r_Board_likeInsert(int rcp_b_num, String m_id) throws Exception {
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("rcp_b_num", rcp_b_num);
 		map.put("m_id", m_id);
@@ -180,6 +183,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 좋아요 테이블에서 좋아요 삭제
 	public void r_Board_likeDelete(int rcp_b_num, String m_id) throws Exception {
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("rcp_b_num", rcp_b_num);
 		map.put("m_id", m_id);
@@ -193,6 +197,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 게시물 좋아요 수치 감소
 	public void r_Board_likeDiscount(int rcp_b_num) throws Exception {
+		
 		logger.info(" DAO : board likeDiscount 시작");
 		
 		sqlSession.update(namespace + ".R_Board_likeDiscount", rcp_b_num);
@@ -202,6 +207,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 게시물 댓글들 불러오기
 	public List<replyVO> r_Board_listReply(String reply_b_type, int reply_b_main_num) throws Exception {
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("reply_b_type", reply_b_type);
 		map.put("reply_b_main_num", reply_b_main_num);
@@ -233,6 +239,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 댓글 등록하기
 	public void r_Board_addReply(replyVO reply) throws Exception {
+		
 		logger.info(" DAO : board addReply 시작");
 		
 		sqlSession.insert(namespace + ".R_Board_addReply", reply);
@@ -242,6 +249,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	
 	// 댓글 삭제하기
 	public void r_Board_deleteReply(int reply_b_num) throws Exception {
+		
 		logger.info(" DAO : board deleteReply 시작");
 		
 		sqlSession.delete(namespace + ".R_Board_deleteReply", reply_b_num);
@@ -276,7 +284,6 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 	// 가져온 컬럼 데이터로 데이터 검색하기
 	@Override
 	public List<Integer> r_Board_searchData(String column, String data) throws Exception {
-		// TODO Auto-generated method stub
 		
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("column", column);
@@ -300,16 +307,15 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 		return sqlSession.selectList(namespace + ".bListCri", cri);
 	}
 	
+	// 갯수 세기
 	@Override
 	public int countBoards(Criteria cri) throws Exception {
-		// TODO Auto-generated method stub
 		return sqlSession.selectOne(namespace+".countBoards", cri);
 	}
 	
-	
+	// 검색리스트 가져오기
 	@Override
 	public List<recipeBoardVO> r_Board_searchNum(List<Integer> rcp_b_nums) throws Exception {
-		// TODO Auto-generated method stub
 		
 		Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
 		map.put("rcp_b_nums", rcp_b_nums);
@@ -370,6 +376,7 @@ public class R_BoardDAOImpl implements R_BoardDAO {
 		}
 		
 		System.out.print(" DAO : getDBColumn 결과 확인 : ");
+		
 		for(String s : columns)
 			System.out.print(s + ", ");
 		
