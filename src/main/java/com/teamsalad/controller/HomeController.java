a0ab76a80aeef891155d0b168385dd72a1e9539a
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2022-01-06 14:03:49 +0900

    Update HomeController.java

--- src/main/java/com/teamsalad/controller/HomeController.java
+++ src/main/java/com/teamsalad/controller/HomeController.java
@@ -11,19 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -38,7 +33,6 @@ public class HomeController {
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public void index(Locale locale, Model model) {
-		
 	}
 	
 }
