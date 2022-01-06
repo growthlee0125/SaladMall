b3803ef650309ee7831c95b6e16cfaf5ab544a21
SJ.LEE <92525310+growthlee0125@users.noreply.github.com>
2022-01-06 20:51:58 +0900

    Update M_LoginServiceImpl.java

--- src/main/java/com/teamsalad/service/M_LoginServiceImpl.java
+++ src/main/java/com/teamsalad/service/M_LoginServiceImpl.java
@@ -29,8 +29,6 @@ public class M_LoginServiceImpl implements M_LoginService {
 	
 	public static final Logger logger = LoggerFactory.getLogger(M_LoginServiceImpl.class);
 	
-	
-	
 	// 로그인
 	@Override
 	public memberVO memberLogin(memberVO mvo) throws Exception {
@@ -40,13 +38,14 @@ public class M_LoginServiceImpl implements M_LoginService {
 		memberVO loginResultMVO = mLdao.memberLogin(mvo);
 		
 		return loginResultMVO;
-		
 	}
 	
 	// 구글 회원가입
 	@Override
 	public void googleJoin(memberVO mvo) throws Exception {	
+		
 		System.out.println(" S: googleJoin(memberVO mvo)");
+		
 		mLdao.googleJoin(mvo);
 	}
 	
@@ -71,19 +70,14 @@ public class M_LoginServiceImpl implements M_LoginService {
 		
 		memberVO ck = mLdao.readMember(vo.getM_id());
 		
-		System.out.println("vo"+vo);
-		System.out.println("ck"+ck);
-		
 		PrintWriter out = response.getWriter();
 		
 		// 가입된 아이디가 없으면
 		if (mLdao.readMember(vo.getM_id()) == null) {
 			out.print("등록되지 않은 아이디입니다.");
 			out.close();
-		}
-		
-		// 가입된 이메일이 아니면
-		else if (!vo.getM_email().equals(ck.getM_email())) {
+		} else if (!vo.getM_email().equals(ck.getM_email())) {
+			// 가입된 이메일이 아니면
 			out.print("등록되지 않은 이메일입니다.");
 			out.close();
 		} else {
@@ -91,55 +85,57 @@ public class M_LoginServiceImpl implements M_LoginService {
 			String pw = "";
 			for (int i = 0; i < 12; i++) {
 				pw += (char) ((Math.random() * 26) + 97);
-			}
-			vo.setM_pw(pw);
-			// 비밀번호 변경
-			mLdao.updatePw(vo);
-			// 비밀번호 변경 메일 발송
-			sendEmail(vo, "findpw");
-
-			out.print("이메일로 임시 비밀번호를 발송하였습니다.");
-			out.close();
 		}
+			
+		vo.setM_pw(pw);
+		
+		// 비밀번호 변경
+		mLdao.updatePw(vo);
+		
+		// 비밀번호 변경 메일 발송
+		sendEmail(vo, "findpw");
 
+		out.print("이메일로 임시 비밀번호를 발송하였습니다.");
+		out.close();
 	}
 	
 	// 비밀번호 찾기 이메일발송
-		@Override
-		public void sendEmail(memberVO vo, String div) throws Exception {
-	        
-	        /* 이메일 보내기 */
-	        String setFrom = "teamsalad2021@gmail.com";
-	        String toMail = vo.getM_email();
-	        String title = "샐러드몰 임시 비밀번호 메일입니다.";
-	        String msg = "";
-	        
-	        System.out.println(toMail);
-	        
-	        if (div.equals("findpw")) {
-	        	System.out.println("진입");
-				msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
-				msg += "<h3 style='color: red;'>";
-				msg += vo.getM_id() + "님의 임시 비밀번호 입니다.</h3>";
-				msg += "<p>임시 비밀번호 : ";
-				msg += vo.getM_pw() + "</p></div>";
-			}
+	@Override
+	public void sendEmail(memberVO vo, String div) throws Exception {
 
-			try {
-				MimeMessage message = mailSender.createMimeMessage();
-				MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-				helper.setFrom(setFrom);
-				helper.setTo(toMail);
-				helper.setSubject(title);
-				helper.setText(msg, true);
-				mailSender.send(message);
-				
-				logger.info("전송완료");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
+		/* 이메일 보내기 */
+		String setFrom = "teamsalad2021@gmail.com";
+		String toMail = vo.getM_email();
+		String title = "샐러드몰 임시 비밀번호 메일입니다.";
+		String msg = "";
+
+		System.out.println(toMail);
+
+		if (div.equals("findpw")) {
+
+			System.out.println("진입");
+
+			msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
+			msg += "<h3 style='color: red;'>";
+			msg += vo.getM_id() + "님의 임시 비밀번호 입니다.</h3>";
+			msg += "<p>임시 비밀번호 : ";
+			msg += vo.getM_pw() + "</p></div>";
 		}
 
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+			helper.setFrom(setFrom);
+			helper.setTo(toMail);
+			helper.setSubject(title);
+			helper.setText(msg, true);
+			mailSender.send(message);
 
+			logger.info("전송완료");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
